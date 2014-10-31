a717cafc14c6cbe4c8857a6f02f39a27d22cf45b
pti1 <pti@gmail.com>
2014-10-31 14:03:03 +0100

    rendered chef-client more modular with introduction of /opt/docker_common_scripts/

--- Chef/Client/startup.sh
+++ Chef/Client/startup.sh
@@ -1,33 +1,9 @@
 #!/usr/bin/env bash
 
-
- 
-ipaddress=`ifconfig eth0 | perl -n -e 'if (m/inet addr:([\d\.]+)/g) { print $1 }'`
-echo "chef serveur ui: https://$ipaddress"
-echo "user is pascal, pwd is pascal"
-
-
-mkdir -p /root/.chef
-
-cat << EOF > /root/.chef/knife.rb
-log_level                :info
-log_location             STDOUT
-node_name                'chef-webui'
-client_key               '/etc/chef-server/chef-webui.pem'
-validation_client_name   'chef-validator'
-validation_key           '/etc/chef-server/chef-validator.pem'
-chef_server_url          'https://chefserver:443'
-syntax_check_cache_path  '/root/.chef/syntax_check_cache'
-cookbook_path [ '/chef-repo/cookbooks' ]
-EOF
-
-cp /etc/chef-server/chef-webui.pem /root/.chef/chef-webui.pem
-
-knife configure client /etc/chef
+cd /opt/docker_common_scripts/
+for f in *.sh; do . ./$f; done
 
 chef-client -d -i 120 -s 5 -c /etc/chef/client.rb
-
-
 /usr/sbin/sshd -D
 
 
