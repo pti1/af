d84c69b217bfd1e3e404b5ae8008925e7bdae650
Your Name <you@example.com>
2014-11-02 18:31:58 +0100

    refactoring

---
+++ Chef/Client/proxy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#TODO
+#astek
+#echo 'Acquire::http::Proxy "http://192.168.0.21:8080";' > /etc/apt/apt.conf.d/proxy
+#echo "export http_proxy=http://192.168.0.21:8080/" > /root/.bashrc
+#echo "export https_proxy=http://192.168.0.21:8080/" > /root/.bashrc
+. /root/.bashrc
+
+
