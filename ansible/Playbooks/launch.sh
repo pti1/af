cb239bbeed1d4b7d16c597d16752ac9c6246bb2d
Your Name <you@example.com>
2014-11-19 10:00:59 +0100

    working on ansible

--- ansible/Playbooks/launch.sh
+++ ansible/Playbooks/launch.sh
@@ -31,6 +31,7 @@ docker stop hadoopmaster
 docker rm hadoopmaster
 docker run -d -t --name hadoopmaster -h hadoopmaster.example.com --dns $(docker inspect -f '{{.NetworkSettings.IPAddress}}' dns) --dns-search example.com --link dns:dns --volumes-from dns  --volumes-from ansibleserver pti1/aptweb_client:secondversion /opt/startup.sh --isproxy=false
 
+
 docker stop hadoopslave1
 docker rm hadoopslave1
 docker run -d -t --name hadoopslave1 -h hadoopslave1.example.com --dns $(docker inspect -f '{{.NetworkSettings.IPAddress}}' dns) --dns-search example.com --link dns:dns --volumes-from dns  --volumes-from ansibleserver pti1/aptweb_client:secondversion /opt/startup.sh --isproxy=false
